bed3ff8b2fec412555f385256efd93b5a7283e21
Iwan <maxim@piesync.com>
2016-12-02 21:58:26 +0100

    Me & the Ocaml Debugger are becoming very good friends

--- 2016/1/iwan.ml
+++ 2016/1/iwan.ml
@@ -1,5 +1,14 @@
 module Utils = struct
   let split ~on str = Str.split (Str.regexp on) str
+
+  exception Empty_List of unit
+  let last lst =
+    let rec last' lst = match lst with
+            | x :: [] -> x
+            | x :: xs -> last' xs
+            | [] -> raise (Empty_List ())
+    in
+    last' lst
 end
 
 type direction = North | East | South | West
@@ -51,24 +60,72 @@ module Coordinate = struct
     | West -> {coord with x = coord.x - count}
     | East -> {coord with x = coord.x + count}
 
+  let travel_path ~direction ~count coord =
+    let rec accumulate direction count coord acc =
+      if count = 0 then acc
+      else let next = travel ~direction ~count:1 coord in
+        accumulate direction (count - 1) next ( next :: acc)
+    in
+    accumulate direction count coord [] |> List.rev
+
+
   let to_blocks {x;y} = abs x + abs y
 end
 
-type location = Location of Coordinate.t * direction
+module Location = struct
+  type t = Location of Coordinate.t * direction
 
+  let intersect_location a b =
+    let eq = fun a b -> match a with
+      | Location (coord, _) -> (match b with
+          | Location (coord2, _) -> coord = coord2) in
+    let candidates = List.filter (fun x -> List.exists (eq x) b) a in
+    match candidates with
+    | [] -> None
+    | x :: xs -> Some x
 
-let step acc curr = let open Instruction in
-  match acc with
-  | Location (coord, dir) ->
-    (match curr with
-     | Instruction (turn, count) ->
-       let next_direction = Turn.turn_direction ~turn ~direction:dir in
-       let next_coord = Coordinate.travel ~direction:next_direction ~count coord in
-       Location (next_coord, next_direction))
+  let to_blocks = function
+    | Location (coord, _) -> Coordinate.to_blocks coord
+end
 
 let pinpoint_destination ~document =
+  let open Instruction in
+  let open Location in
+  let walk acc curr =
+    match acc with
+    | Location (coord, dir) ->
+      (match curr with
+       | Instruction (turn, count) ->
+         let next_direction = Turn.turn_direction ~turn ~direction:dir in
+         let next_coord = Coordinate.travel ~direction:next_direction ~count coord in
+         Location (next_coord, next_direction))
+  in
   let starting_point = Location (Coordinate.center, North) in
   let steps = List.map Instruction.from_string @@ Utils.split ~on:", " document in
-  let destination = List.fold_left step starting_point steps in
-  match destination with
-   | Location (coord, _) -> Coordinate.to_blocks coord
+  List.fold_left walk starting_point steps
+  |> Location.to_blocks
+
+let take_a_taxi ~document =
+  let open Instruction in
+  let open Location in
+  let rec drive current directions visited =
+    match directions with
+    | x :: xs ->
+      (match current with
+       | Location (coord, dir) ->
+         (match x with
+          | Instruction (turn, count) ->
+            let next_direction = Turn.turn_direction ~turn ~direction:dir in
+            let path = Coordinate.travel_path ~direction:next_direction ~count coord in
+            let location_path = List.map (fun coord -> Location (coord, next_direction)) path in
+            let next_coord = Utils.last path in
+            let next_loc = Location (next_coord, next_direction) in
+            let intersected = intersect_location location_path visited in
+            match intersected with
+            | Some loc -> Location.to_blocks loc
+            | None -> drive next_loc xs (List.concat [visited; location_path])))
+    | [] -> 0
+  in
+  let starting_point = Location (Coordinate.center, North) in
+  let directions = List.map Instruction.from_string @@ Utils.split ~on:", " document in
+  drive starting_point directions [starting_point]
